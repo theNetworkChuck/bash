116d8b8790487fd1509a96afceb7286cd8242da0
theNetworkChuck <47433490+theNetworkChuck@users.noreply.github.com>
2022-04-21 13:47:46 -0500

    Update getrichquick.sh

--- getrichquick.sh
+++ getrichquick.sh
@@ -2,5 +2,11 @@
 
 #It's time to get RICH!!
 
-#first, let's create a simple script asking for our name and age. 
+#first, let's create a simple script asking for our name and age and store it in a variable named "name" and "age". Ask for use input.
+#put the code below 
+
+
+
+
+#Now, print out the result as "Hello ______, you are ____ years old."
 
